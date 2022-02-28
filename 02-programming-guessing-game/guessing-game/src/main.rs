81104b09bdca2ceddfe111e324cc506f3eed2cbe
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-02-28 23:39:53 +0530

    recieve user input and display

--- 02-programming-guessing-game/guessing-game/src/main.rs
+++ 02-programming-guessing-game/guessing-game/src/main.rs
@@ -1,3 +1,17 @@
+// Import input/output library
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("---Guess the number 🔢!---");
+    println!("Input your guess : ");
+
+    // Create a mutable variable that is currently bound to a new, empty instance of a String.
+    let mut guess = String::new();
+
+    // Recieving user input
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to readline! 🤕");
+
+    println!("You Guessed: {}", guess);
 }
