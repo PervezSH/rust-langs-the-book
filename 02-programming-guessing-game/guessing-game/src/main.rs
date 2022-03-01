845bc6b3ed01afbcce39d5afc7213f9344bd9908
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-01 22:55:31 +0530

    generate random number 🔢
    - using random number library

--- 02-programming-guessing-game/guessing-game/src/main.rs
+++ 02-programming-guessing-game/guessing-game/src/main.rs
@@ -1,14 +1,22 @@
-// Import input/output library
+// include the input/output functionality from the standard library
 use std::io;
+// include random number generator from random number library
+use rand::Rng;
 
 fn main() {
     println!("---Guess the number 🔢!---");
     println!("Input your guess : ");
 
-    // Create a mutable variable that is currently bound to a new, empty instance of a String.
+    // prepare a non-deterministic random number generator:
+    let mut rng = rand::thread_rng();
+    // generate random number between 0 - 100
+    let secret_number = rng.gen_range(1..101);
+    println!("The secret number is {}", secret_number);
+
+    // create a mutable variable that is currently bound to a new, empty instance of a String.
     let mut guess = String::new();
 
-    // Recieving user input
+    // recieving user input
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to readline! 🤕");
