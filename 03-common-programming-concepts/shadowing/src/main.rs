01134d8f666584bd41257c1743a7238f3fff83e0
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-03 23:34:11 +0530

    difference b/w mut and shadowing

--- 03-common-programming-concepts/shadowing/src/main.rs
+++ 03-common-programming-concepts/shadowing/src/main.rs
@@ -7,4 +7,12 @@ fn main() {
         println!("Value of x in inner scope is : {}", x);
     }
     println!("Value of x is : {}", x);
+
+    // difference between mut and shadowing
+    // this will work
+    let spaces = "          ";
+    let spaces = spaces.len(); // The first spaces variable is a string type and the second spaces variable is a number type
+                               // however, this will not work
+    let mut spaces = "          ";
+    spaces = spaces.len();
 }
