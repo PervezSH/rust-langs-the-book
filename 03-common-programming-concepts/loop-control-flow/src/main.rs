887f9c6290812bb77a8cea31eb13cc38dd1b6664
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-05 23:53:13 +0530

    break and continue control flow in loops

--- 03-common-programming-concepts/loop-control-flow/src/main.rs
+++ 03-common-programming-concepts/loop-control-flow/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    // break and continue control flow in loops
+    break_and_continue();
+}
+
+fn break_and_continue() {
+    let mut counter = 0;
+    // labeled loop
+    'outer_loop: loop {
+        let mut inner_counter = 0;
+        loop {
+            if inner_counter == 2 {
+                println!("Inner Counter : {}", inner_counter);
+                counter += 2;
+                break 'outer_loop;
+            }
+            inner_counter += 1;
+        }
+    }
+    println!("Outer Counter : {}", counter);
 }
