f917abf0dbd5fb3ce9ec01b4c0f0181621cf0317
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-05 23:23:06 +0530

    diff statement and expression

--- 03-common-programming-concepts/statements-and-expression/src/main.rs
+++ 03-common-programming-concepts/statements-and-expression/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // a statement
+    let x = 10;
+
+    // A new scope block created with curly braces is an expression
+    let y = {
+        let a = 5;
+        a + 5 // expression do not include semicolons, if you add one, you turn it into statement
+    };
+
+    println!("Value of x and y = {}, {}", x, y);
 }
