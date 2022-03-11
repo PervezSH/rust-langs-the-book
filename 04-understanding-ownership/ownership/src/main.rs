c10cd59d580075f92d3c1c8daa692d9d95ea5b6c
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-11 23:26:03 +0530

    feat(ownership): clone ✨

--- 04-understanding-ownership/ownership/src/main.rs
+++ 04-understanding-ownership/ownership/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     // Move
     implementMove();
+    // Clone
+    implementClone();
 }
 
 fn implementMove() {
@@ -8,4 +10,10 @@ fn implementMove() {
     let s2 = s1;
     // here rust make s1, no longer valid, to ensure memory safet, double free error
     println!("{}, world!", s1);
+}
+
+fn implementClone() {
+    let s1 = String::from("hello");
+    let s2 = s1.clone(); // deeply copies heap data of s1
+    println!("{} from s1, {} from, world!", s1, s2);
 }
