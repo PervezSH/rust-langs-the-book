65e8db8b792956a74748233a5fd24893641b3855
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-19 00:09:55 +0530

    feat(struct): without self param ✨

--- 05-using-structs/methods/src/main.rs
+++ 05-using-structs/methods/src/main.rs
@@ -12,6 +12,16 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         other.width < self.width && other.height < self.height
     }
+    // associated function that don't have self
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+    fn is_square(&self) -> bool {
+        self.height == self.width
+    }
 }
 
 fn main() {
@@ -26,4 +36,7 @@ fn main() {
 
     println!("Area of rect1 is {}", rect1.area());
     println!("Can rect1 hold rect2 {}", rect1.can_hold(&rect2));
+
+    let rect3 = Rectangle::square(10);
+    println!("Is rect3 a square? {}", rect3.is_square());
 }
