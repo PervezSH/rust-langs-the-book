649e9ab7fd3a3e13392a28b2a9e08b4691e91420
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-19 00:02:30 +0530

    feat(struct): method as parameters ✨

--- 05-using-structs/methods/src/main.rs
+++ 05-using-structs/methods/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    // method with parameters
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        other.width < self.width && other.height < self.height
+    }
 }
 
 fn main() {
@@ -15,6 +19,11 @@ fn main() {
         width: 80,
         height: 20,
     };
+    let rect2 = Rectangle {
+        width: 70,
+        height: 10,
+    };
 
     println!("Area of rect1 is {}", rect1.area());
+    println!("Can rect1 hold rect2 {}", rect1.can_hold(&rect2));
 }
