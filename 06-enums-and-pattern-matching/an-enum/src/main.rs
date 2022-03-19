a092516cb6b68326358487d291f544e13d157f8b
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-19 23:45:42 +0530

    feat(enums): define methods on enums ✨

--- 06-enums-and-pattern-matching/an-enum/src/main.rs
+++ 06-enums-and-pattern-matching/an-enum/src/main.rs
@@ -1,13 +1,19 @@
-#[derive(Debug)]
+#[derive(Debug)] // so we can inspect
 enum IpAddrKind {
     V4(u8, u8, u8, u8),
     V6(String),
+}
+
+impl IpAddrKind {
+    fn display_ip(&self) {
+        println!("Ip address is {:?}", self);
+    }
 }
 
 fn main() {
     let ipv4 = IpAddrKind::V4(127, 0, 0, 1);
     let ipv6 = IpAddrKind::V6(String::from("::1"));
 
-    println!("Version 4 Ip address {:?}", ipv4);
-    println!("Version 6 Ip address {:?}", ipv6);
+    ipv4.display_ip();
+    ipv6.display_ip();
 }
